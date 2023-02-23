df0ac45af28c793e00fe8182878aa9d409071d11
henry-shih4 <100496651+henry-shih4@users.noreply.github.com>
2023-02-23 13:28:09 -0600

    change redirect https

--- src/components/ShoesContext.js
+++ src/components/ShoesContext.js
@@ -4,9 +4,9 @@ import useFetch from "../useFetch";
 const ShoesContext = createContext();
 
 function ShoesProvider(props) {
-  const url = "http://node-env.eba-fuimik5e.us-east-1.elasticbeanstalk.com/";
+  const url = "https://node-env.eba-fuimik5e.us-east-1.elasticbeanstalk.com/";
   const { data, loading, error } = useFetch(
-    "http://node-env.eba-fuimik5e.us-east-1.elasticbeanstalk.com/api/v1/shoes"
+    "https://node-env.eba-fuimik5e.us-east-1.elasticbeanstalk.com/api/v1/shoes"
   );
   return (
     <ShoesContext.Provider value={[data, url, loading, error]}>
