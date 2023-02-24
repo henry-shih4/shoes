1e08e8e658e9db69f7b4a35c3f3f18a52358831b
henry-shih4 <100496651+henry-shih4@users.noreply.github.com>
2023-02-24 01:37:59 -0600

    new backend
    hosted on adapatable

--- src/components/ShoesContext.js
+++ src/components/ShoesContext.js
@@ -4,9 +4,9 @@ import useFetch from "../useFetch";
 const ShoesContext = createContext();
 
 function ShoesProvider(props) {
-  const url = "http://node-env.eba-fuimik5e.us-east-1.elasticbeanstalk.com/";
+  const url = "https://rebound-shoes-api.adaptable.app/";
   const { data, loading, error } = useFetch(
-    "http://node-env.eba-fuimik5e.us-east-1.elasticbeanstalk.com/api/v1/shoes"
+    "https://rebound-shoes-api.adaptable.app/api/v1/shoes"
   );
   return (
     <ShoesContext.Provider value={[data, url, loading, error]}>
