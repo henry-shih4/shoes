4b62a15318c1ca49e68bf1e7872ab0a58834c298
Henry Shih <100496651+henry-shih4@users.noreply.github.com>
2023-02-23 18:09:16 -0600

    Update ShoesContext.js

--- src/components/ShoesContext.js
+++ src/components/ShoesContext.js
@@ -4,10 +4,8 @@ import useFetch from "../useFetch";
 const ShoesContext = createContext();
 
 function ShoesProvider(props) {
-  const url = "https://node-env.eba-fuimik5e.us-east-1.elasticbeanstalk.com/";
-  const { data, loading, error } = useFetch(
-    "https://node-env.eba-fuimik5e.us-east-1.elasticbeanstalk.com/api/v1/shoes"
-  );
+  const url = "http://node-env.eba-fuimik5e.us-east-1.elasticbeanstalk.com/";
+  const { data, loading, error } = useFetch(`${url}api/v1/shoes`);
   return (
     <ShoesContext.Provider value={[data, url, loading, error]}>
       {props.children}
